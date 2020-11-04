f468c5f6188e927e28def5c11597dfd15cbf51c7
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-11-04 09:24:41 +0100

    Add project files.

---
+++ 13Feladat/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace _13Feladat
+{
+    partial class frmFo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBeolvas = new System.Windows.Forms.Button();
+            this.btnElso = new System.Windows.Forms.Button();
+            this.btnMasodik = new System.Windows.Forms.Button();
+            this.btnHarmadik = new System.Windows.Forms.Button();
+            this.btnNegyedik = new System.Windows.Forms.Button();
+            this.btnOtodik = new System.Windows.Forms.Button();
+            this.btnHatodik = new System.Windows.Forms.Button();
+            this.btnMagyarazat = new System.Windows.Forms.Button();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tslblLeiras = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "13. Feladat";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.btnMagyarazat);
+            this.panel1.Controls.Add(this.btnHatodik);
+            this.panel1.Controls.Add(this.btnOtodik);
+            this.panel1.Controls.Add(this.btnNegyedik);
+            this.panel1.Controls.Add(this.btnHarmadik);
+            this.panel1.Controls.Add(this.btnMasodik);
+            this.panel1.Controls.Add(this.btnElso);
+            this.panel1.Controls.Add(this.btnBeolvas);
+            this.panel1.Location = new System.Drawing.Point(45, 105);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(487, 331);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnBeolvas
+            // 
+            this.btnBeolvas.Location = new System.Drawing.Point(188, 3);
+            this.btnBeolvas.Name = "btnBeolvas";
+            this.btnBeolvas.Size = new System.Drawing.Size(121, 47);
+            this.btnBeolvas.TabIndex = 2;
+            this.btnBeolvas.Text = "Beolvasás";
+            this.btnBeolvas.UseVisualStyleBackColor = true;
+            this.btnBeolvas.Click += new System.EventHandler(this.btnBeolvas_Click);
+            // 
+            // btnElso
+            // 
+            this.btnElso.Enabled = false;
+            this.btnElso.Location = new System.Drawing.Point(46, 68);
+            this.btnElso.Name = "btnElso";
+            this.btnElso.Size = new System.Drawing.Size(121, 47);
+            this.btnElso.TabIndex = 3;
+            this.btnElso.Text = "1.";
+            this.btnElso.UseVisualStyleBackColor = true;
+            this.btnElso.Click += new System.EventHandler(this.btnElso_Click);
+            // 
+            // btnMasodik
+            // 
+            this.btnMasodik.Enabled = false;
+            this.btnMasodik.Location = new System.Drawing.Point(46, 133);
+            this.btnMasodik.Name = "btnMasodik";
+            this.btnMasodik.Size = new System.Drawing.Size(121, 47);
+            this.btnMasodik.TabIndex = 4;
+            this.btnMasodik.Text = "2.";
+            this.btnMasodik.UseVisualStyleBackColor = true;
+            this.btnMasodik.Click += new System.EventHandler(this.btnMasodik_Click);
+            // 
+            // btnHarmadik
+            // 
+            this.btnHarmadik.Enabled = false;
+            this.btnHarmadik.Location = new System.Drawing.Point(46, 199);
+            this.btnHarmadik.Name = "btnHarmadik";
+            this.btnHarmadik.Size = new System.Drawing.Size(121, 47);
+            this.btnHarmadik.TabIndex = 5;
+            this.btnHarmadik.Text = "3.";
+            this.btnHarmadik.UseVisualStyleBackColor = true;
+            this.btnHarmadik.Click += new System.EventHandler(this.btnHarmadik_Click);
+            // 
+            // btnNegyedik
+            // 
+            this.btnNegyedik.Enabled = false;
+            this.btnNegyedik.Location = new System.Drawing.Point(327, 68);
+            this.btnNegyedik.Name = "btnNegyedik";
+            this.btnNegyedik.Size = new System.Drawing.Size(121, 47);
+            this.btnNegyedik.TabIndex = 6;
+            this.btnNegyedik.Text = "4.";
+            this.btnNegyedik.UseVisualStyleBackColor = true;
+            this.btnNegyedik.Click += new System.EventHandler(this.btnNegyedik_Click);
+            // 
+            // btnOtodik
+            // 
+            this.btnOtodik.Enabled = false;
+            this.btnOtodik.Location = new System.Drawing.Point(327, 133);
+            this.btnOtodik.Name = "btnOtodik";
+            this.btnOtodik.Size = new System.Drawing.Size(121, 47);
+            this.btnOtodik.TabIndex = 7;
+            this.btnOtodik.Text = "5.";
+            this.btnOtodik.UseVisualStyleBackColor = true;
+            this.btnOtodik.Click += new System.EventHandler(this.btnOtodik_Click);
+            // 
+            // btnHatodik
+            // 
+            this.btnHatodik.Enabled = false;
+            this.btnHatodik.Location = new System.Drawing.Point(327, 199);
+            this.btnHatodik.Name = "btnHatodik";
+            this.btnHatodik.Size = new System.Drawing.Size(121, 47);
+            this.btnHatodik.TabIndex = 8;
+            this.btnHatodik.Text = "6.";
+            this.btnHatodik.UseVisualStyleBackColor = true;
+            this.btnHatodik.Click += new System.EventHandler(this.btnHatodik_Click);
+            // 
+            // btnMagyarazat
+            // 
+            this.btnMagyarazat.Location = new System.Drawing.Point(188, 281);
+            this.btnMagyarazat.Name = "btnMagyarazat";
+            this.btnMagyarazat.Size = new System.Drawing.Size(121, 47);
+            this.btnMagyarazat.TabIndex = 9;
+            this.btnMagyarazat.Text = "Feladatleírás";
+            this.btnMagyarazat.UseVisualStyleBackColor = true;
+            this.btnMagyarazat.Click += new System.EventHandler(this.btnMagyarazat_Click);
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(490, 12);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(82, 27);
+            this.btnBezar.TabIndex = 10;
+            this.btnBezar.Text = "Bezárás";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            this.btnBezar.Click += new System.EventHandler(this.btnBezar_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslblLeiras});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 439);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tslblLeiras
+            // 
+            this.tslblLeiras.Name = "tslblLeiras";
+            this.tslblLeiras.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmFo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmFo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "13. Feladat";
+            this.panel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnMagyarazat;
+        private System.Windows.Forms.Button btnHatodik;
+        private System.Windows.Forms.Button btnOtodik;
+        private System.Windows.Forms.Button btnNegyedik;
+        private System.Windows.Forms.Button btnHarmadik;
+        private System.Windows.Forms.Button btnMasodik;
+        private System.Windows.Forms.Button btnElso;
+        private System.Windows.Forms.Button btnBeolvas;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tslblLeiras;
+    }
+}
+
---
+++ 13Feladat/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _13Feladat
+{
+    public partial class frmFo : Form
+    {
+        private List<int> Lista = new List<int>();
+        private bool hiba = false;
+        public frmFo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBeolvas_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader olvas = new StreamReader("adatok.txt");
+                while (!olvas.EndOfStream)
+                {
+                    try
+                    {
+                        Lista.Add(int.Parse(olvas.ReadLine()));
+                    }
+                    catch (Exception)
+                    {
+                        hiba = true;
+                    }
+                    
+                }
+                olvas.Close();
+                if (hiba)
+                {
+                    tslblLeiras.Text = $"A fájl hibás adatot is tartalmazott, ami figyelmen kívül lett hagyva.";
+                }else tslblLeiras.Text = "A beolvasás sikeres, feloldódott a tömbi gomb is.";
+                btnElso.Enabled = true;
+                btnMasodik.Enabled = true;
+                btnHarmadik.Enabled = true;
+                btnNegyedik.Enabled = true;
+                btnOtodik.Enabled = true;
+                btnHatodik.Enabled = true;
+                btnBeolvas.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Váratlan hiba történt! A program leáll. ({ex})", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void btnMagyarazat_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1. Összegzés tétele -> átlag; 2. Hányadik a 7143; " +
+                "3. Hányszor fordul elő az 1170; 4. Van-e benne 8876; 5. Minimum; 6. Maximum",
+                "Leírás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnBezar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnElso_Click(object sender, EventArgs e)
+        {
+            double osszeg, atlag, db;
+            osszeg = 0; atlag = 0; db = 0;
+            foreach (var l in Lista)
+            {
+                osszeg += l;
+                db++;
+            }
+            atlag = osszeg / db;
+            MessageBox.Show($"1. Feladat: {Math.Round(atlag, 2)}", "1.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnMasodik_Click(object sender, EventArgs e)
+        {
+            if (!Lista.Contains(7143))
+            {
+                MessageBox.Show("A lista nem tartalmaz ilyen számot.");
+            }
+            else
+            {
+                for (int i = 0; i < Lista.Count; i++)
+                {
+                    if (Lista[i] == 7143)
+                    {
+                        MessageBox.Show($"2. Feladat: A 7143 a {i + 1}.", "2.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void btnHarmadik_Click(object sender, EventArgs e)
+        {
+            int hanyszor = 0;
+            foreach (var l in Lista)
+            {
+                if (l == 1170)
+                {
+                    hanyszor++;
+                }
+            }
+            MessageBox.Show($"Az 1170 {hanyszor}-szer/szor fordul elő", "3.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnNegyedik_Click(object sender, EventArgs e)
+        {
+            if (Lista.Contains(8876))
+            {
+                MessageBox.Show($"A 8876-ot tartalmazza az adatok.txt", "4.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }else MessageBox.Show($"A 8876-ot nem tartalmazza az adatok.txt", "4.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnOtodik_Click(object sender, EventArgs e)
+        {
+            int minimum = Lista[0];
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (minimum > Lista[i])
+                {
+                    minimum = Lista[i];
+                }
+            }
+            MessageBox.Show($"A minimum {minimum}", "5.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnHatodik_Click(object sender, EventArgs e)
+        {
+            int maximum = Lista[0];
+            for (int i = 0; i < Lista.Count; i++)
+            {
+                if (maximum < Lista[i])
+                {
+                    maximum = Lista[i];
+                }
+            }
+            MessageBox.Show($"A maximum {maximum}", "5.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
